6587f10c77611d12435858a004e1e1a5ba25e5e9
Olivier Brunet <olibrunet@free.fr>
2020-10-01 08:47:48 +0200

    Updates

--- component---src-pages-page-2-js-5cdbeb9b80c16eb8d6fe.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{1127:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),c=a(137),l=a(1122);t.default=function(){return o.a.createElement(l.a,null,o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(c.a,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-page-2-js-5cdbeb9b80c16eb8d6fe.js.map
